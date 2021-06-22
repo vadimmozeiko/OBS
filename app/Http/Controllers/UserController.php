0ec50435844d9fa92d546c3a2cfb9a7062a9c16a
Vadim M <vadim.mozeiko@gmail.com>
2021-06-22 17:05:00 +0300

    profile edit validations #5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Validator;
 
 class UserController extends Controller
 {
 
     public function index()
     {
-
-        return view('user.index');
+        $user = User::where('name', Auth::user()->name)->get();
+        return view('user.index', ['user' => $user]);
     }
 
 
@@ -27,24 +29,42 @@ class UserController extends Controller
     }
 
 
-    public function show(User $customer)
+    public function show(User $user)
     {
 
     }
 
-    public function edit(User $customer)
+    public function edit(User $user)
     {
-        //
+        return view('user.edit', ['user' => $user]);
     }
 
 
-    public function update(Request $request, User $customer)
+    public function update(Request $request, User $user)
     {
-        //
+        $validator = Validator::make($request->all(),
+            [
+                'user_name' => ['required', 'string', 'max:255'],
+                'user_address' => ['required', 'string', 'max:255'],
+                'user_phone' => ['required', 'numeric', 'min:11'],
+//                'user_email' => ['required', 'string', 'email:rfc', 'max:255', 'unique:users']
+            ]);
+
+        if ($validator->fails()) {
+            $request->flash();
+            return redirect()->back()->withErrors($validator);
+        }
+
+        $user->name = $request->user_name;
+        $user->email = $request->user_email;
+        $user->address = $request->user_address;
+        $user->phone = $request->user_phone;
+        $user->save();
+        return redirect()->route('user.index');
     }
 
 
-    public function destroy(User $customer)
+    public function destroy(User $user)
     {
         //
     }
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,10 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-@foreach($customer as $person)
-{{$person->name}}
-{{$person->email}}
-{{$person->created_at}}
-
-@endforeach
+    @foreach($user as $person)
+        <div class="container d-flex flex-wrap justify-content-around">
+            <div class="col-sm-6 col-md-4 col-lg-4 mb-5">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h2>{{$person->name}}</h2>
+                        <p>{{$person->email}}</p>
+                        <p>{{$person->address}}</p>
+                        <p>{{$person->phone}}</p>
+                        <a class="card-link m-1 btn btn-primary" href="{{route('user.edit', $person)}}">Edit details</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
 @endsection
